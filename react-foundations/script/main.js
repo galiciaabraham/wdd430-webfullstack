006a64167edb25c892f8c06b638271df1b33bb7e
galiciaabraham <galicia.abraham@hotmail.com>
2024-09-16 17:54:52 -0600

    Transformed my imperative DOM code to declarative React code

--- react-foundations/script/main.js
+++ react-foundations/script/main.js
@@ -1,7 +1,4 @@
 const app = document.getElementById('app');
 
-let header = document.createElement('h1');
-const text = "I've created this new H1 element using DOM"
-header.textContent = text;
-
-app.appendChild(header);
+const root = ReactDOM.createRoot(app);
+root.render (<h1>"I've created this new H1 element using DOM"</h1>);
